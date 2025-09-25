597a92f6dd0b0f68e1259f5560d29b84174ba301
karthikn <karthikn@opentext.com>
2025-09-25 09:52:48 +0530

    sample app for build

---
+++ Module1.vb
@@ -0,0 +1,7 @@
+Module Module1
+
+    Sub Main()
+        Console.WriteLine("Hello, .NET Sample Application!")
+    End Sub
+
+End Module
---
+++ program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MySampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, .NET Sample Application!");
+        }
+    }
+}
